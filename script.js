1634299cc49bb12f1babbcfc283d7b66ccbc22ca
Dev Parmar <89907756+devparmarr@users.noreply.github.com>
2023-07-30 23:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,76 +1,120 @@
-//your code here
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('gameContainer');
-  const scoreElement = document.getElementById('score');
-  const width = 400;
-  const height = 400;
-  const pixelSize = 40;
-  const rows = height / pixelSize;
-  const cols = width / pixelSize;
-  const totalPixels = rows * cols;
 
-  let snake = [
-    { row: 20, col: 1 },
-    { row: 20, col: 2 },
-    { row: 20, col: 3 }
-  ];
+    document.addEventListener('DOMContentLoaded', () => {
+      const container = document.getElementById('gameContainer');
+      const scoreElement = document.getElementById('score');
+      const width = 400;
+      const height = 400;
+      const pixelSize = 40;
+      const rows = height / pixelSize;
+      const cols = width / pixelSize;
+      const totalPixels = rows * cols;
 
-  let direction = 'right';
-  let food = {};
-  let score = 0;
+      let snake = [
+        { row: 20, col: 1 },
+        { row: 20, col: 2 },
+        { row: 20, col: 3 }
+      ];
 
-  function createGrid() {
-    for (let i = 0; i < totalPixels; i++) {
-      const pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      pixel.setAttribute('id', `pixel${i + 1}`);
-      container.appendChild(pixel);
-    }
-  }
+      let direction = 'right';
+      let food = {};
+      let score = 0;
 
-  function createSnake() {
-    for (let i = 0; i < snake.length; i++) {
-      const { row, col } = snake[i];
-      const pixelIndex = (row - 1) * cols + col;
-      const snakePixel = document.getElementById(`pixel${pixelIndex}`);
-      snakePixel.classList.add('snakeBodyPixel');
-    }
-  }
+      function createGrid() {
+        for (let i = 0; i < totalPixels; i++) {
+          const pixel = document.createElement('div');
+          pixel.classList.add('pixel');
+          pixel.setAttribute('id', `pixel${i + 1}`);
+          container.appendChild(pixel);
+        }
+      }
 
-  function generateFood() {
-    const availablePixels = Array.from(Array(totalPixels).keys());
-    const snakePixels = snake.map(pixel => (pixel.row - 1) * cols + pixel.col);
-    const emptyPixels = availablePixels.filter(pixel => !snakePixels.includes(pixel));
-    const randomIndex = Math.floor(Math.random() * emptyPixels.length);
-    const foodPixelIndex = emptyPixels[randomIndex];
-    const foodPixel = document.getElementById(`pixel${foodPixelIndex}`);
-    foodPixel.classList.add('food');
-    food.row = Math.floor(foodPixelIndex / cols) + 1;
-    food.col = (foodPixelIndex % cols) + 1;
-  }
+      function createSnake() {
+        for (let i = 0; i < snake.length; i++) {
+          const { row, col } = snake[i];
+          const pixelIndex = (row - 1) * cols + col;
+          const snakePixel = document.getElementById(`pixel${pixelIndex}`);
+          snakePixel.classList.add('snakeBodyPixel');
+        }
+      }
 
-  function moveSnake() {
-    const head = Object.assign({}, snake[0]);
+      function generateFood() {
+        const availablePixels = Array.from(Array(totalPixels).keys());
+        const snakePixels = snake.map(pixel => (pixel.row - 1) * cols + pixel.col);
+        const emptyPixels = availablePixels.filter(pixel => !snakePixels.includes(pixel));
+        const randomIndex = Math.floor(Math.random() * emptyPixels.length);
+        const foodPixelIndex = emptyPixels[randomIndex];
+        const foodPixel = document.getElementById(`pixel${foodPixelIndex}`);
+        foodPixel.classList.add('food');
+        food.row = Math.floor(foodPixelIndex / cols) + 1;
+        food.col = (foodPixelIndex % cols) + 1;
+      }
 
-    switch (direction) {
-      case 'up':
-        head.row--;
-        break;
-      case 'down':
-        head.row++;
-        break;
-      case 'left':
-        head.col--;
-        break;
-      case 'right':
-        head.col++;
-        break;
-    }
+      function moveSnake() {
+        const head = Object.assign({}, snake[0]);
 
-    if (head.row < 1 || head.row > rows || head.col < 1 || head.col > cols) {
-      gameOver();
-      return;
-    }
+        switch (direction) {
+          case 'up':
+            head.row--;
+            break;
+          case 'down':
+            head.row++;
+            break;
+          case 'left':
+            head.col--;
+            break;
+          case 'right':
+            head.col++;
+            break;
+        }
 
-    const newHeadPixelIndex = (head.row - 1) * cols + head.col;
-    const newHeadPixel = document
+        if (head.row < 1 || head.row > rows || head.col < 1 || head.col > cols) {
+          gameOver();
+          return;
+        }
+
+        const newHeadPixelIndex = (head.row - 1) * cols + head.col;
+        const newHeadPixel = document.getElementById(`pixel${newHeadPixelIndex}`);
+        newHeadPixel.classList.add('snakeBodyPixel');
+        snake.unshift(head);
+
+        if (food.row === head.row && food.col === head.col) {
+          newHeadPixel.classList.remove('food');
+          score++;
+          scoreElement.innerText = `Score: ${score}`;
+          generateFood();
+        } else {
+          const tail = snake.pop();
+          const tailPixelIndex = (tail.row - 1) * cols + tail.col;
+          const tailPixel = document.getElementById(`pixel${tailPixelIndex}`);
+          tailPixel.classList.remove('snakeBodyPixel');
+        }
+      }
+
+      function gameOver() {
+        clearInterval(gameLoop);
+        alert('Game Over! Your Score: ' + score);
+      }
+
+      createGrid();
+      createSnake();
+      generateFood();
+
+      let gameLoop = setInterval(moveSnake, 200);
+
+      document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+          case 'ArrowUp':
+            if (direction !== 'down') direction = 'up';
+            break;
+          case 'ArrowDown':
+            if (direction !== 'up') direction = 'down';
+            break;
+          case 'ArrowLeft':
+            if (direction !== 'right') direction = 'left';
+            break;
+          case 'ArrowRight':
+            if (direction !== 'left') direction = 'right';
+            break;
+        }
+      });
+    });
